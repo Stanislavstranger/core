58ba66bf47116108b8b79db259964fbbd99e593f
Stanislav <stanislavstranger@gmail.com>
2025-08-20 16:29:27 +0300

    feat: add h1 on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import { ServiceList } from '@/features/services-list/pub/services-list';
 
 export default async function Home() {
   return (
-    <div className="flex min-h-screen flex-col p-8">
+    <main className="flex min-h-screen flex-col p-8">
+      <h1 className="text-3xl font-bold">Список сервисов</h1>
       <CreateServiceForm revalidatePagePath="/" className="max-w-[300px] mb-5" />
       <ServiceList revalidatePagePath="/" />
-    </div>
+    </main>
   );
 }
